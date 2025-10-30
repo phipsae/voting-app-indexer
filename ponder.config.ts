6684b99f590edaac0bdd1d5893a6ea6b572728cb
phipsae <philip.krause88@yahoo.de>
2025-10-30 21:41:55 +0300

    clean, remove mainnent, and change address to new contract

--- ponder.config.ts
+++ ponder.config.ts
@@ -3,103 +3,18 @@ import { parseAbiItem } from "viem";
 
 import { ZkVotingAbi, ZkVotingFactoryAbi } from "./abis/ExampleContractAbi";
 
-const ZK_VOTING_FACTORY_ADDRESS_MAINNET =
-  "0xDD05E565B844469cb02bcCdD50f94C92aC8bBA13";
 const ZK_VOTING_FACTORY_ADDRESS_BASE =
-  "0x69eD9786967830b97Fd4e7CA1DD5C2a8460C8C11";
-const START_BLOCK_MAINNET = 23376465;
-const START_BLOCK_BASE = 37388451;
+  "0x678043eD0f934dC07F2ed5BB7B29F49bda091f0a";
+const START_BLOCK_BASE = 37529913;
 
 export default createConfig({
   chains: {
-    mainnet: {
-      id: 1,
-      rpc: process.env.PONDER_RPC_MAINNET!,
-    },
-    baseSepolia: {
-      id: 84532,
-      rpc: process.env.PONDER_RPC_BASE_SEPOLIA!,
-    },
     base: {
       id: 8453,
       rpc: process.env.PONDER_RPC_BASE!,
     },
-    sepolia: {
-      id: 11155111,
-      rpc: process.env.PONDER_RPC_SEPOLIA!,
-    },
   },
   contracts: {
-    /// baseSepolia
-    //   ZkVotingFactory: {
-    //     chain: "baseSepolia",
-    //     abi: ZkVotingFactoryAbi,
-    //     address: ZK_VOTING_FACTORY_ADDRESS,
-    //     startBlock: START_BLOCK,
-    //   },
-    //   ZkVoting: {
-    //     chain: "baseSepolia",
-    //     abi: ZkVotingAbi,
-    //     address: factory({
-    //       address: ZK_VOTING_FACTORY_ADDRESS,
-    //       event: parseAbiItem(
-    //         "event VotingCreated(address indexed creator, address indexed voting, string question)"
-    //       ),
-    //       parameter: "voting",
-    //       // (Optional) scan the factory’s whole history for children:
-    //       startBlock: START_BLOCK,
-    //     }),
-
-    //     startBlock: START_BLOCK,
-    //   },
-    // },
-    /// Sepolia //////
-    //   ZkVotingFactory: {
-    //     chain: "sepolia",
-    //     abi: ZkVotingFactoryAbi,
-    //     address: ZK_VOTING_FACTORY_ADDRESS,
-    //     startBlock: START_BLOCK,
-    //   },
-    //   ZkVoting: {
-    //     chain: "sepolia",
-    //     abi: ZkVotingAbi,
-    //     address: factory({
-    //       address: ZK_VOTING_FACTORY_ADDRESS,
-    //       event: parseAbiItem(
-    //         "event VotingCreated(address indexed creator, address indexed voting, string question)"
-    //       ),
-    //       parameter: "voting",
-    //       // (Optional) scan the factory’s whole history for children:
-    //       startBlock: START_BLOCK,
-    //     }),
-
-    //     startBlock: START_BLOCK,
-    //   },
-    // },
-    ///////////////
-    /// Mainnet ///
-    ///////////////
-    // ZkVotingFactoryMainnet: {
-    //   chain: "mainnet",
-    //   abi: ZkVotingFactoryAbi,
-    //   address: ZK_VOTING_FACTORY_ADDRESS_MAINNET,
-    //   startBlock: START_BLOCK_MAINNET,
-    // },
-    // ZkVotingMainnet: {
-    //   chain: "mainnet",
-    //   abi: ZkVotingAbi,
-    //   address: factory({
-    //     address: ZK_VOTING_FACTORY_ADDRESS_MAINNET,
-    //     event: parseAbiItem(
-    //       "event VotingCreated(address indexed creator, address indexed voting, string question)"
-    //     ),
-    //     parameter: "voting",
-    //     // (Optional) scan the factory’s whole history for children:
-    //     startBlock: START_BLOCK_MAINNET,
-    //   }),
-
-    //   startBlock: START_BLOCK_MAINNET,
-    // },
     ///////////////
     /// Base ///
     ///////////////
